c0081e2cd9d43a63b1eb33e0b65aa3c61d0165bc
David Cote <dmc1991@icloud.com>
2024-06-16 22:20:32 -0400

    Adds component files

---
+++ src/components/PhotoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PhotoList = () => {
+  return (
+    <div class="photo-container">
+      <h2>Results</h2>
+      <ul></ul>
+    </div>
+  );
+};
+
+export default PhotoList;
